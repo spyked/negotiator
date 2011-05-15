990fa3ec79bd0bd5ec4938bdf4dc4454ddaae73e
Lucian Mogoșanu <lucian.mogosanu@gmail.com>
2011-05-15 23:29:11 +0300

    More architectural changes/additions:
    * Negotiation: added generic negotiation based on State
    * Agent: adapted Agent to changed Negotiation type
    
    Other: added Plot module to plot utility functions and others

---
+++ src/Plot.hs
@@ -0,0 +1,18 @@
+module Plot where
+
+cleanFile :: FilePath -> IO ()
+cleanFile path = writeFile path ""
+
+writeOptions :: FilePath -> IO ()
+writeOptions path = appendFile path "set key off\nset grid\nset ticslevel 0\n"
+
+writePoints :: (Show a, Show b) => FilePath -> [(a,b)] -> IO ()
+writePoints path ps = mapM_ writePoint ps
+    where
+    writePoint (x,y) = appendFile path $ show x ++ " " ++ show y ++ "\n"
+
+writeFunc :: (Show a, Show b) => FilePath -> (a -> b) -> [a] -> IO ()
+writeFunc path f inps = writePoints path ps >> appendFile path "end\n\n"
+    where
+    ps = zip [1 .. length inps] $ map f inps
+
