e46e2cd8edec13878e7e4aa7b59dab4c3c96ac6b
Lucian Mogoșanu <lucian.mogosanu@gmail.com>
2011-05-31 19:56:11 +0300

    server improvements and simple site
    * a simple site, using jQuery. this will be used to create a web
      interface.
    * web service is still work in progress.

--- src/BargainServer.hs
+++ src/BargainServer.hs
@@ -1,13 +1,30 @@
 module Main where
 
 import Control.Monad
+import Control.Monad.Trans (liftIO)
+import qualified Text.Html as H
 import Happstack.Server
+import System.FilePath ((</>))
+
+siteDir :: String
+siteDir = "site"
 
 htmlContent :: Monad m => FilePath -> m String
 htmlContent = asContentType "text/html"
 
+negotiationSession :: ServerPart Response
+negotiationSession = do
+    -- liftIO $ print "bla"
+    serveFile htmlContent (siteDir </> "session.html")
+
+getHumanName :: ServerPart Response
+getHumanName = ok $ toResponse "DeepThought"
+
 main :: IO ()
 main = simpleHTTP nullConf $ msum
     [ 
-        nullDir >> serveFile htmlContent "index.html"
+        dir "session" $ negotiationSession,
+        dir "name" $ getHumanName,
+        nullDir >> serveFile htmlContent (siteDir </> "index.html"),
+        anyPath $ serveFile htmlContent (siteDir </> "404.html") >>= notFound
     ]
