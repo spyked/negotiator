22ef35458f1dcf4b56070e95402910822b840983
Lucian Mogoșanu <lucian.mogosanu@gmail.com>
2011-06-02 18:42:37 +0300

    interface improvement and session support

--- src/BargainServer.hs
+++ src/BargainServer.hs
@@ -8,6 +8,7 @@ import System.FilePath ((</>))
 siteDir :: String
 siteDir = "site"
 
+-- content types
 htmlContent :: Monad m => FilePath -> m String
 htmlContent = asContentType "text/html"
 
@@ -20,27 +21,45 @@ cssContent = asContentType "text/css"
 gifContent :: Monad m => FilePath -> m String
 gifContent = asContentType "image/gif"
 
-negotiationSession :: ServerPart Response
-negotiationSession = do
-    -- liftIO $ print "bla"
-    serveFile htmlContent (siteDir </> "session.html")
-
-getHumanName :: ServerPart Response
-getHumanName = ok $ toResponse "DeepThought"
-
+-- serve spin button data
 spinButtonData :: ServerPart Response
 spinButtonData = msum
     [ 
     dir "ui.spinner.js" $ serveFile jsContent (siteDir </> "ui.spinner.js")
     ]
 
+-- serve various resources
+getHumanName :: ServerPart Response
+getHumanName = do
+    name <- lookCookieValue "username"
+    ok $ toResponse name
 
+negotiationSession :: ServerPart Response
+negotiationSession = msum 
+    [
+        do methodM POST
+           decodeBody postPolicy
+           username <- look "username"
+           -- TODO: initialize state
+           addCookie Session (mkCookie "username" username)
+           serveSession
+    ,   do username <- lookCookieValue "username" 
+           serveSession
+           -- TODO: handle state
+    ,   do serveFile htmlContent (siteDir </> "nosession.html")
+    ]
+    where
+    serveSession = serveFile htmlContent (siteDir </> "session.html")
+    postPolicy = defaultBodyPolicy "/tmp/" 0 256 256
+
+-- main loop
 main :: IO ()
 main = simpleHTTP nullConf $ msum
     [ 
         dir "session" $ negotiationSession,
         dir "name" $ getHumanName,
-            nullDir >> serveFile htmlContent (siteDir </> "index.html"),
+        dir "index" $ serveFile htmlContent (siteDir </> "index.html"),
+        nullDir >> serveFile htmlContent (siteDir </> "index.html"),
         spinButtonData,
         anyPath $ serveFile htmlContent (siteDir </> "404.html") >>= notFound
     ]
